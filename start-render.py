6ea89c28f75ffc215163466e4f5c196932c1da23
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-09-29 10:40:59 +0200

    Add script launching command line rendering for all projects

---
+++ start-render.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import ntpath
+import subprocess
+import sys
+import webbrowser
+
+# assign directory
+directory = 'projects'
+
+# iterate over files in
+# that directory
+counter = 1
+for filename in os.listdir(directory):
+    projectFile = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(projectFile):
+        fname = ntpath.basename(projectFile)
+        # ensure destination render does not exists
+        outName = os.path.splitext(filename)[0] + '.mp4'
+        outputFile = os.path.join('renders', outName)
+        if os.path.isfile(outputFile):
+            print("Render file " + outputFile + " already exists, aborting")
+            sys.exit()
+        print("Processing project: " + fname + "...")
+        subprocess.call(['kdenlive', "--render", "--synchronous", projectFile, outputFile])
+        print("Processing project: " + fname + "... DONE")
+
+subprocess.call(['./compare-renders.py'])
+
